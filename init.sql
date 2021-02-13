92e2f6ee8181470e7ab338dd63369a38098bc823
fox0 <red-fox0@yandex.ru>
2021-02-13 12:40:15 +0700

    work

--- init.sql
+++ init.sql
@@ -1,30 +1,40 @@
 create schema if not exists "ABITURA";
 
-create table if not exists "ABITURA"."SUPER_DIFF" (
-  --id pk
-  --created now
-  --uuid
-  "ACTION" varchar(30),
-  "TABLE_NAME" varchar(500) not null
+--drop table "ABITURA"."SUPER_DIFF";
+create table if not exists "ABITURA"."SUPER_DIFF"
+(
+    "ID"            serial primary key,
+    "CREATED"       timestamp default current_timestamp not null,
+    "TG_OP"         varchar(6)                          not null,
+    "TG_TABLE_NAME" varchar(100)                        not null,
+    "UUID"          varchar(30)                         not null
 );
 
-CREATE FUNCTION "ABITURA".make_diff() RETURNS trigger AS $$
-  -- DECLARE
-  --  action varchar(30);
-  BEGIN
-    insert into "ABITURA"."SUPER_DIFF" ("ACTION", "TABLE_NAME") values (TG_OP, TG_TABLE_NAME);
-    RETURN NEW;
-  END;
-$$ LANGUAGE plpgsql;
+drop function "ABITURA".make_diff cascade;
+create function "ABITURA".make_diff() returns trigger as
+$$
+declare
+    l_uuid varchar(30);
+begin
+    if tg_op = 'DELETE' then
+        l_uuid := old."UUID";
+    else
+        l_uuid := new."UUID";
+    end if;
+    insert into "ABITURA"."SUPER_DIFF" ("TG_OP", "TG_TABLE_NAME", "UUID")
+    values (tg_op, tg_table_name, l_uuid);
+    return new;
+end;
+$$ language plpgsql;
 
-create table if not exists "ABITURA"."SUPER_KAF" (
-  "KAF" varchar(30) primary key,
-  "NAME" varchar(500) not null
+create table if not exists "ABITURA"."SUPER_KAF"
+(
+    "UUID" varchar(30) primary key,
+    "NAME" varchar(500) not null
 );
-CREATE TRIGGER diff AFTER INSERT OR UPDATE OR DELETE ON "ABITURA"."SUPER_KAF"
-  FOR EACH ROW EXECUTE PROCEDURE "ABITURA".make_diff();
-
-insert into "ABITURA"."SUPER_KAF" (
-    "KAF",
-    "NAME")
-values ('123', 'dfdfdf');
+--drop trigger diff on "ABITURA"."SUPER_KAF";
+create trigger diff
+    after insert or update or delete
+    on "ABITURA"."SUPER_KAF"
+    for each row
+execute procedure "ABITURA".make_diff();
